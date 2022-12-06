027bbe3c88a6fc1aefe356694beeb494773d0b84
LassiMik <lassi@vartiaiset.com>
2022-12-06 22:04:57 +0200

    asd

--- src/components/Customerlist.js
+++ src/components/Customerlist.js
@@ -8,9 +8,11 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
+import DeleteIcon from "@mui/icons-material/Delete";
 import MenuIcon from '@mui/icons-material/Menu';
 import Navigation from './Navigation'
 import Traininglist from './Traininglist'
+import Editcustomer from "./Editcustomer";
 
 function Customerlist() {
     const [customers, setCustomers] = useState([]);
@@ -23,18 +25,58 @@ function Customerlist() {
         fetch("https://customerrest.herokuapp.com/api/customers")
             .then((response) => response.json())
             .then((data) => setCustomers(data.content));
-    }
+    };
+
+    const deleteCustomer = (link) => {
+        fetch(link, { method: "DELETE" }).then((response) => {
+            if (response.ok) {
+                fetchCustomers();
+            }
+        });
+    };
+    const updateCustomer = (updateCustomer, link) => {
+        fetch(link, {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(updateCustomer),
+        }).then((response) => {
+          if (response.ok) {
+            fetchCustomers();
+          }
+        });
+      };
+
+
     const [columnDefs, setColumnDefs] = useState([
-        { field: "firstname", sortable: true, filter: true  },
-        { field: "lastname", sortable: true, filter: true  },
-        { field: "email", sortable: true, filter: true  },
-        { field: "phone", sortable: true, filter: true  },
-        { field: "streetaddress", sortable: true, filter: true  },
-        { field: "postcode", sortable: true, filter: true  },
-        { field: "city", sortable: true, filter: true  },
-    ])
+        { field: "firstname", sortable: true, filter: true, },
+        { field: "lastname", sortable: true, filter: true },
+        { field: "email", sortable: true, filter: true },
+        { field: "phone", sortable: true, filter: true },
+        { field: "streetaddress", sortable: true, filter: true },
+        { field: "postcode", sortable: true, filter: true },
+        { field: "city", sortable: true, filter: true },
+        {
+            headerName: "",
+            width: 100,
+            field: "_links.self.href",
+            cellRenderer: (params) => (
+
+                <IconButton color="error" onClick={() => deleteCustomer(params.value)}>
+                    <DeleteIcon />
+                </IconButton>
+            ),
+        },
+        {
+            headerName: "",
+            width: 100,
+            field: "_links.self.href",
+            cellRenderer: (params) => (
+                <Editcustomer updateCustomer={updateCustomer} params={params} />
+            ),
+        },
+    ]);
     return (
-        <> 
+        <>
             <div
                 style={{ height: 600, width: "100%" }}
                 className="ag-theme-material"
--- src/components/Traininglist.js
+++ src/components/Traininglist.js
@@ -2,33 +2,28 @@ import React, { useState, useEffect } from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
-import DayJS from 'react-dayjs';
+import dayjs from 'dayjs';
 
 function Traininglist() {
-    const training = [
-        { activity: 'Spinning', date: DayJS('2022-12-06T05:31:00.517+00:00'), duration: 60, customer: 'John Johnson' },
-        { activity: 'Spinning', date: DayJS('2022-12-06T05:31:00.517+00:00'), duration: 60, customer: 'John Johnson' }
-    ]
-    /*const [training, setTraining] = ([
-        { activity: 'Spinning', date: 'asd', duration: 60, customer: 'John Johnson' },
-        { activity: 'Spinning', date: 'asd', duration: 60, customer: 'John Johnson' }
-    ]);*/
-    console.log(training);
-    /*
+    const [trainings, setTrainings] = useState([]);
+    const [training, setTraining] = React.useState({ activity: "", date: "", duration: "", customerFirstname: "", customerLastname: "" });
+    const gridRef = React.useRef();
+
     useEffect(() => {
         fetchTraining();
     }, []);
-    */
-    /*const fetchTraining = () => {
-        fetch("https://customerrest.herokuapp.com/api/customers")
+
+    const fetchTraining = () => {
+        fetch("https://customerrest.herokuapp.com/gettrainings")
             .then((response) => response.json())
-            .then((data) => console.log(data.content.links));
-    }*/
+            .then((data) => setTrainings(data));
+    }
     const [columnDefs, setColumnDefs] = useState([
         { field: "activity" },
         { field: "date" },
         { field: "duration" },
-        { field: "customer" },
+        { field: "customer.firstname", headerName: "Customer Name" },
+        { field: "customer.lastname", headerName: "" }
     ])
     return (
         <> 
@@ -37,7 +32,7 @@ function Traininglist() {
                 className="ag-theme-material"
             >
                 <AgGridReact
-                    rowData={training}
+                    rowData={trainings}
                     columnDefs={columnDefs}
                 >
                 </AgGridReact>
